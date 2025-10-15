97e99363820bb8d4c8df534c0d76fd0dd355295d
385944 <a385944@uach.mx>
2025-10-15 07:42:00 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
 import streamlit as st
 import math
 
-st.title("calcular  de figuras trigonometricas🧮")
+st.title("calculadora de figuras trigonometricas")
+
+# 1. Selección de figura
+figura = st.selectbox("Selecciona una figura geométrica", ["Circulo","Triángulo", "Rectangulo", "Cuadrado"])
+
+# 2. Mostrar parámetros relevantes
+if figura == "Triángulo":
+    st.subheader("Parámetros del triángulo")
+    base = st.number_input("Base (b)", min_value=0.0, format="%.2f")
+    altura = st.number_input("Altura (h)", min_value=0.0, format="%.2f")
+    lado_a = st.number_input("Lado a", min_value=0.0, format="%.2f")
+    lado_b = st.number_input("Lado b", min_value=0.0, format="%.2f")
+    lado_c = st.number_input("Lado c", min_value=0.0, format="%.2f")
+
